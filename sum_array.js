da6f9a70038616f3708b3d7bcf0f7e0234f9a5a4
allirani007 <84637284+allirani007@users.noreply.github.com>
2021-05-31 22:35:24 +0530

    Create sum_array.js

---
+++ sum_array.js
@@ -0,0 +1,44 @@
+//Sum of all numbers in an array
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+
+var arr= userInput[0].split(" ");
+
+var sum=0;
+//console.log(arr.length);
+
+function abc(arr)
+{
+    for(i=0;i<arr.length;i++)
+    {
+        sum += +arr[i]
+
+    }
+    //var sum=arr.reduce(function(a,b){return a+b;},0);
+
+   console.log(sum) }
+
+
+
+// var array = [1, 2, 3, 4, 5];
+    
+    // Getting sum of numbers
+    //var sum = arr.reduce(function(a, b){ return a + b;  }, 0);
+    
+    console.log(abc(arr)); // Prints: 15
+
+//console.log(arr.reduce((a, b) => a + b, 0) )
+
+});
+
